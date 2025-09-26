36837319d16b5c3c578de605c20ce890f75cbdbb
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-09-26 04:05:49 +0000

    sdsd

--- binary_trees.h
+++ binary_trees.h
@@ -38,6 +38,17 @@ typedef struct levelorder_queue_s
 	struct levelorder_queue_s *next;
 } levelorder_queue_t;
 
+/**
+ * struct avl_data_s - Struct to hold AVL validation info
+ * @height: Height of the subtree
+ * @is_AVL: 1 if subtree is AVL, 0 otherwise
+ */
+typedef struct avl_data_s
+{
+    int height;
+    int is_AVL;
+} avl_data_t;
+
 /* Printing helper function */
 void binary_tree_print(const binary_tree_t *);
 
@@ -82,5 +93,6 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
+int binary_tree_is_avl(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
