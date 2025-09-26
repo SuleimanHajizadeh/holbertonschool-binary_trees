6802a3a297e86eb5b16368b1cfffc17da8845d36
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-09-26 10:12:52 +0000

    sdsd

---
+++ 123-avl_remove.c
@@ -0,0 +1,8 @@
+#include "binary_trees.h"
+
+/* placeholder so file is not empty */
+avl_t *avl_remove(avl_t *root, int value)
+{
+    (void)value;  /* avoid unused parameter warning */
+    return (root);
+}
