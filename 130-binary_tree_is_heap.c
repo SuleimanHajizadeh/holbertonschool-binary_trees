fc374a5a3289029da6dc40216c9caa9aa670966b
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-09-26 10:37:10 +0000

    sdsd

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
-#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * count_nodes - Counts the number of nodes in a binary tree
@@ -7,41 +8,40 @@
  *
  * Return: Number of nodes
  */
-static int count_nodes(const binary_tree_t *tree)
+size_t count_nodes(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-
 	return (1 + count_nodes(tree->left) + count_nodes(tree->right));
 }
 
 /**
  * is_complete - Checks if a binary tree is complete
  * @tree: Pointer to the root node
- * @index: Index of the current node
- * @node_count: Total nodes in the tree
+ * @index: Current index in array representation
+ * @nodes: Total number of nodes
  *
  * Return: 1 if complete, 0 otherwise
  */
-static int is_complete(const binary_tree_t *tree, int index, int node_count)
+int is_complete(const binary_tree_t *tree, size_t index, size_t nodes)
 {
 	if (!tree)
 		return (1);
 
-	if (index >= node_count)
+	if (index >= nodes)
 		return (0);
 
-	return (is_complete(tree->left, 2 * index + 1, node_count) &&
-	        is_complete(tree->right, 2 * index + 2, node_count));
+	return (is_complete(tree->left, 2 * index + 1, nodes) &&
+		is_complete(tree->right, 2 * index + 2, nodes));
 }
 
 /**
- * is_heap_util - Checks if the tree maintains max-heap property
+ * is_max_heap - Checks the Max Heap property
  * @tree: Pointer to the root node
  *
- * Return: 1 if max-heap, 0 otherwise
+ * Return: 1 if valid Max Heap, 0 otherwise
  */
-static int is_heap_util(const binary_tree_t *tree)
+int is_max_heap(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (1);
@@ -52,26 +52,29 @@ static int is_heap_util(const binary_tree_t *tree)
 	if (tree->right && tree->n < tree->right->n)
 		return (0);
 
-	return (is_heap_util(tree->left) && is_heap_util(tree->right));
+	return (is_max_heap(tree->left) && is_max_heap(tree->right));
 }
 
 /**
- * binary_tree_is_heap - Checks if a binary tree is a valid Max Binary Heap
+ * binary_tree_is_heap - Checks if a binary tree is a Max Binary Heap
  * @tree: Pointer to the root node
  *
- * Return: 1 if valid Max Binary Heap, 0 otherwise
+ * Return: 1 if tree is a Max Binary Heap, 0 otherwise
  */
 int binary_tree_is_heap(const binary_tree_t *tree)
 {
-	int node_count;
+	size_t nodes;
 
 	if (!tree)
 		return (0);
 
-	node_count = count_nodes(tree);
+	nodes = count_nodes(tree);
 
-	if (!is_complete(tree, 0, node_count))
+	if (!is_complete(tree, 0, nodes))
 		return (0);
 
-	return (is_heap_util(tree));
+	if (!is_max_heap(tree))
+		return (0);
+
+	return (1);
 }
